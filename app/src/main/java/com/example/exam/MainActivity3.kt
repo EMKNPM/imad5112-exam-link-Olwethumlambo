89271b6a060ecefca3f0ab41a6cb0c73f8cd8de3
Olwethumlambo <ST10529421@myemeris.edu.za>
2026-06-10 14:03:00 +0200

    changes

--- app/src/main/java/com/example/exam/MainActivity3.kt
+++ app/src/main/java/com/example/exam/MainActivity3.kt
@@ -55,7 +55,7 @@ class MainActivity3 : AppCompatActivity() {
 
         tvDetails.text=output
 
-        
+
         btnBack.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
