b8895521c7fc64222cf34807198c7e913fcd4aa8
Olwethumlambo <ST10529421@myemeris.edu.za>
2026-06-10 12:22:58 +0200

    second screen
    declarations and adding the last screen

--- app/src/main/java/com/example/exam/MainActivity2.kt
+++ app/src/main/java/com/example/exam/MainActivity2.kt
@@ -1,16 +1,53 @@
 package com.example.exam
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+    //Declarations
+    private lateinit var btnAdd : Button
+    private lateinit var button2 : Button
+    private lateinit var button3 : Button
+    private lateinit var tvShelter : TextView
+    private lateinit var tvFood : TextView
+    private lateinit var tvSafety : TextView
+    private lateinit var btnNext : Button
+
+    //create an array of items in details
+    val item = arrayOf("Tent", "Marshmallows", "Flashlight")
+
+    //create an array of categories
+    val category = arrayOf("Shelter", "Food", "Safety")
+
+    //create an array of quantity
+    val quantity = arrayOf(1, 2, 3)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+        //typecasting
+        btnAdd = findViewById(R.id.btnAdd)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+        tvShelter = findViewById(R.id.tvShelter)
+        tvFood = findViewById(R.id.tvFood)
+        tvSafety = findViewById(R.id.tvSafety)
+        btnNext = findViewById(R.id.btnNext)
+
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, MainActivity3 ::class.java)
+            startActivity(intent)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/exam/MainActivity3.kt
+++ app/src/main/java/com/example/exam/MainActivity3.kt
@@ -1,40 +1,16 @@
 package com.example.exam
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.os.Handler
-import android.os.Looper
-class MainActivity : AppCompatActivity() {
 
-    //Declarations
-    private lateinit var tvTittle : TextView
-
-
-
+class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
-        //typecasting
-        tvTittle = findViewById(R.id.tvTitle)
-
-
-
-
-
-
-
-
+        setContentView(R.layout.activity_main3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
